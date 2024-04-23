56e09e85d8088b557421b64c831acad7e2ad6a62
Kashish <kashishjain01885@gmail.com>
2024-04-23 15:49:12 +0530

    feat: implemented image picker from gallery and camera

--- Desktop/reactNative/my-app/src/Components/UserProfile.js
+++ Desktop/reactNative/my-app/src/Components/UserProfile.js
@@ -1,14 +1,81 @@
-import { View, Text, StyleSheet ,Image} from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet ,Image, TouchableOpacity} from 'react-native'
+import React, { useState } from 'react'
 import imagePath from '../constants/imagePath'
 import colors from '../styles/colors'
 import { moderateScale, verticalScale, width } from '../styles/scaling'
+import * as ImagePicker from "expo-image-picker"
+import CustomModal from './Modal'
 
 export default function UserProfile() {
+  const [modalVisible,setModalVisible]=useState(false)
+  const [image,setImage]=useState(null)
+  const uploadImage= async (mode)=>{ 
+    try{
+      let result={}
+      if(mode ==="gallery"){
+        await ImagePicker.requestMediaLibraryPermissionsAsync()
+        result=await ImagePicker.launchImageLibraryAsync({
+          mediaTypes:ImagePicker.MediaTypeOptions.Images,
+          allowsEditing:true,
+          aspect:[1,1],
+          quality:1
+        })
+
+      }
+      else{
+          await ImagePicker.requestCameraPermissionsAsync() 
+          result=await ImagePicker.launchCameraAsync({
+          cameraType:ImagePicker.CameraType.back,
+          allowsEditing:true,
+          aspect:[1,1],
+          quality:1
+        })
+      }
+      if(!result.canceled){
+        await saveImage(result.assets[0].uri)
+      }
+    }
+    catch(error){
+      alert("Error in uploading the image" + error)
+      setModalVisible(false)
+    }
+  }
+  const saveImage = async (imageUrl) => {
+    try {
+      if (!imageUrl) {
+        console.log("no url present")
+        setImage(null);
+      } else {
+        console.log("uri present" ,imageUrl)
+        setImage(imageUrl);
+      }
+      setModalVisible(false);
+    } catch (error) {
+      throw error;
+    }
+  };
+  const removeImage=async()=>{
+    try{
+      console.log("remove function")
+      saveImage(null)
+    }
+    catch(error){
+      setModalVisible(false)
+    }
+  }
+  const openModal=()=>{
+    setModalVisible(true)
+  }
+  const closeModal=()=>{
+    setModalVisible(false) 
+  }
   return (
     <View style={styles.imgContainer}>
-      <Image source={imagePath.userProfile} style={styles.img}/>
+      <Image source={{ uri: image }} style={styles.img} />
+      <TouchableOpacity hitSlop={{ top: 25, bottom: 25, left: 15, right: 15 }} onPress={()=>openModal()}>
       <Image source={imagePath.edit} style={styles.img1}/>
+      </TouchableOpacity>
+      <CustomModal visible={modalVisible} closeModal={closeModal} onCameraPress={()=>uploadImage()} onGalleryPress={()=>uploadImage("gallery")} onPressRemove={()=>removeImage()}/>
     </View>
   )
 }
@@ -28,8 +95,8 @@ const styles=StyleSheet.create({
         height:20,
         resizeMode:'contain',
         position:'absolute',
-        top:verticalScale(70),
-        right:moderateScale(118)
+        bottom:verticalScale(1),
+        // right:moderateScale(118)
         
     }
 })
